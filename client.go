a22b3def6010e794360ca9c625cbd164e73ee66b
bilginyuksel <bilgin.yuksel96@gmail.com>
2021-12-04 11:17:49 +0300

    Add tests for client.go parsing methods:

--- client.go
+++ client.go
@@ -47,31 +47,33 @@ func New(opts ...Option) *Client {
 
 // GetJSON execute a get method with the given request and then unmarshal the json response body
 func (c *Client) GetJSON(ctx context.Context, request *Request, response interface{}) error {
-	return c.ParseJSON(ctx, request.Path(http.MethodGet), response)
+	return c.ParseJSON(ctx, request.Method(http.MethodGet), response)
 }
 
 // GetXML execute a get method with the given request and then unmarshal the xml response body
 func (c *Client) GetXML(ctx context.Context, request *Request, response interface{}) error {
-	return c.ParseXML(ctx, request.Path(http.MethodGet), response)
+	return c.ParseXML(ctx, request.Method(http.MethodGet), response)
 }
 
-// ParseJSON ...
+// ParseJSON send a request with given request properties
+// Read the body and run json unmarshaler to fill the given response
 func (c *Client) ParseJSON(ctx context.Context, request *Request, response interface{}) error {
-	res, err := c.Do(ctx, request)
-	if err != nil {
-		return err
-	}
-	defer res.Body.Close()
-
-	responseBytes, err := io.ReadAll(res.Body)
-	if err != nil {
-		return nil
-	}
-	return json.Unmarshal(responseBytes, response)
+	return c.Parse(ctx, request, response, func(bodyBytes []byte, response interface{}) error {
+		return json.Unmarshal(bodyBytes, response)
+	})
 }
 
-// ParseXML ...
+// ParseXML send a request with the given request properties
+// Read the body and run xml unmarshaler to fill the given response
 func (c *Client) ParseXML(ctx context.Context, request *Request, response interface{}) error {
+	return c.Parse(ctx, request, response, func(bodyBytes []byte, response interface{}) error {
+		return xml.Unmarshal(bodyBytes, response)
+	})
+}
+
+// Parse send a request with the given request properties
+// Read the body with the given parser function
+func (c *Client) Parse(ctx context.Context, request *Request, response interface{}, parser func(bodyBytes []byte, response interface{}) error) error {
 	res, err := c.Do(ctx, request)
 	if err != nil {
 		return err
@@ -80,9 +82,9 @@ func (c *Client) ParseXML(ctx context.Context, request *Request, response interf
 
 	responseBytes, err := io.ReadAll(res.Body)
 	if err != nil {
-		return nil
+		return err
 	}
-	return xml.Unmarshal(responseBytes, response)
+	return parser(responseBytes, response)
 }
 
 // Do Execute an http request with the given request
--- client_test.go
+++ client_test.go
@@ -11,8 +11,90 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var ctx = context.Background()
+
+func TestGetJSON_SuccessfulRequest_ExpectGETMethodInRequest(t *testing.T) {
+	s, recorder := aduket.NewServer(http.MethodGet, "/test")
+	cli := client.New(client.WithHost(s.URL))
+	req := cli.NewRequest(ctx).Path("/test").
+		Method(http.MethodPatch).
+		AddHeader("X-R", "req")
+
+	_ = cli.GetJSON(ctx, req, nil)
+	// if there is no request captured on GET aduket.URL/test, it will fail
+	recorder.AssertHeaderContains(t, http.Header{"X-R": []string{"req"}})
+}
+
+func TestGetXML_SuccessfulRequest_ExpectGETMethodInRequest(t *testing.T) {
+	s, recorder := aduket.NewServer(http.MethodGet, "/test")
+	cli := client.New(client.WithHost(s.URL))
+	req := cli.NewRequest(ctx).Path("/test").
+		Method(http.MethodPatch).
+		AddHeader("X-R", "req")
+
+	_ = cli.GetXML(ctx, req, nil)
+	// if there is no request captured on GET aduket.URL/test, it will fail
+	recorder.AssertHeaderContains(t, http.Header{"X-R": []string{"req"}})
+}
+
+func TestParseJSON_SuccessfulRequest_FillGivenResponseStruct(t *testing.T) {
+	type Test struct {
+		Firstname string `json:"firstname"`
+		Lastname  string `json:"lastname"`
+	}
+	expectedTestStruct := &Test{
+		Firstname: "firstname",
+		Lastname:  "lastname",
+	}
+	s, _ := aduket.NewServer(http.MethodGet, "/orders", aduket.JSONBody(expectedTestStruct))
+	cli := client.New(client.WithHost(s.URL))
+	req := cli.NewRequest(ctx).Path("/orders")
+
+	var actualTestStruct Test
+	err := cli.ParseJSON(ctx, req, &actualTestStruct)
+
+	assert.Nil(t, err)
+	assert.Equal(t, expectedTestStruct, &actualTestStruct)
+}
+
+func TestParseXML_SuccessfulRequest_FillGivenResponseStruct(t *testing.T) {
+	type Test struct {
+		Firstname string `xml:"firstname"`
+		Lastname  string `xml:"lastname"`
+	}
+	expectedTestStruct := &Test{
+		Firstname: "firstname",
+		Lastname:  "lastname",
+	}
+	s, _ := aduket.NewServer(http.MethodGet, "/test", aduket.XMLBody(expectedTestStruct))
+	cli := client.New(client.WithHost(s.URL))
+	req := cli.NewRequest(ctx).Path("/test")
+
+	var actualTestStruct Test
+	err := cli.ParseXML(ctx, req, &actualTestStruct)
+
+	assert.Nil(t, err)
+	assert.Equal(t, expectedTestStruct, &actualTestStruct)
+}
+
+func TestParse_CorruptedBody_ReturnErr(t *testing.T) {
+	s, _ := aduket.NewServer(http.MethodGet, "/orders", aduket.CorruptedBody())
+	cli := client.New(client.WithHost(s.URL))
+	req := cli.NewRequest(ctx).Path("/orders")
+
+	err := cli.Parse(ctx, req, nil, nil)
+	assert.NotNil(t, err)
+}
+
+func TestParse_DoFailed_ReturnErr(t *testing.T) {
+	cli := client.New(client.WithHost("local:host:3000"))
+	req := cli.NewRequest(ctx) // to fail new request with context
+
+	err := cli.Parse(ctx, req, nil, nil)
+	assert.NotNil(t, err)
+}
+
 func TestDo_ValidRequest_CaptureTheHTTPRequestWithTheGivenParameters(t *testing.T) {
-	ctx := context.Background()
 	s, recorder := aduket.NewServer(http.MethodPost, "/basket/1")
 	cli := client.New(client.WithHost(s.URL))
 
@@ -42,7 +124,6 @@ func TestDo_ValidRequest_CaptureTheHTTPRequestWithTheGivenParameters(t *testing.
 }
 
 func TestDo_WrongHTTPMethod_ReturnErr(t *testing.T) {
-	ctx := context.Background()
 	cli := client.New(client.WithHost("http://localhost:3000"))
 	req := cli.NewRequest(ctx).Method(",") // to fail new request with context
 
@@ -52,7 +133,6 @@ func TestDo_WrongHTTPMethod_ReturnErr(t *testing.T) {
 }
 
 func TestDo_InvalidHost_ReturnErr(t *testing.T) {
-	ctx := context.Background()
 	cli := client.New(client.WithHost("local:host:3000"))
 	req := cli.NewRequest(ctx) // to fail new request with context
 
